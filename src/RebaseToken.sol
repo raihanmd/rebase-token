17ea4f070d01475ed294ce5ee28fbd4b61a19978
raihanmd <mraihanf471@gmail.com>
2026-01-10 20:06:22 +0700

    update gitignore

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract RebaseToken {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RebaseToken is ERC20 {
+  constructor() ERC20("RebaseToken", "RBS") {}
+}
